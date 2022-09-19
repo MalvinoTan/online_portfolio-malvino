1e99cc262e57bf3735ed933ed9b2c647d40b8f55
MalvinoTan <malvinoaustintanura@gmail.com>
2022-09-19 09:25:23 +0700

    Change to class component to SideBar

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,73 +1,79 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
 import profile from "../../assets/img/profile.jpeg";
 
 /** Styles */
 import styles from "./style.module.css";
 
-const SideBar = () => {
-
-    const menuData = [
-        {
-            title: "Home",
-            path: "/",
-        },
-        {
-            title: "About",
-            path: "/about",
-        },
-        {
-            title: "Experience",
-            path: "/experience",
-        },
-        {
-            title: "Education",
-            path: "/education",
-        },
-        {
-            title: "Skills",
-            path: "/skills",
-        },
-        {
-            title: "Interests",
-            path: "/interests",
-        },
-        {
-            title: "Awards",
-            path: "/awards",
-        },
-        {
-            title: "Organizations",
-            path: "/organizations",
-        }
-    ];
-
-    return (
-        <>
-            <div className={styles.sidebar}>
-                <div className={styles.img}>
-                    <img src={profile} alt="profile" />
-                </div>
+class SideBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuData: [
                 {
-                    menuData.map((data, dataIdx) => (
-                        <NavLink
-                            to={data.path}
-                            className={styles.sidebar_item}
-                            key={dataIdx}
-                            style={({ isActive }) => {
-                                return {
-                                    paddingRight: isActive ? "0.5rem" : "",
-                                    color: isActive ? "#0268b1" : "",
-                                    borderRight: isActive ? " 8px solid #0583d2" : "",
-                                };
-                            }}>
-                            {data.title}
-                        </NavLink>
-                    ))
+                    title: "Home",
+                    path: "/",
+                },
+                {
+                    title: "About",
+                    path: "/about",
+                },
+                {
+                    title: "Experience",
+                    path: "/experience",
+                },
+                {
+                    title: "Education",
+                    path: "/education",
+                },
+                {
+                    title: "Skills",
+                    path: "/skills",
+                },
+                {
+                    title: "Interests",
+                    path: "/interests",
+                },
+                {
+                    title: "Awards",
+                    path: "/awards",
+                },
+                {
+                    title: "Organizations",
+                    path: "/organizations",
                 }
-            </div>
-        </>
+            ]
+        };
+    }
 
-    );
+    render() {
+        return (
+            <>
+                <div className={styles.sidebar}>
+                    <div className={styles.img}>
+                        <img src={profile} alt="profile" />
+                    </div>
+                    {
+                        this.state.menuData.map((data, dataIdx) => (
+                            <NavLink
+                                to={data.path}
+                                className={styles.sidebar_item}
+                                key={dataIdx}
+                                style={({ isActive }) => {
+                                    return {
+                                        paddingRight: isActive ? "0.5rem" : "",
+                                        color: isActive ? "#0268b1" : "",
+                                        borderRight: isActive ? " 8px solid #0583d2" : "",
+                                    };
+                                }}>
+                                {data.title}
+                            </NavLink>
+                        ))
+                    }
+                </div>
+            </>
+        )
+    }
 }
 
 export default SideBar;
